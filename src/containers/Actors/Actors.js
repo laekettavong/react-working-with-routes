a85d890f33c947a6c2b83794059284d0d9c988b1
Khoutsaneh Kettavong <laekettavong@Khoutsanehs-iMac.local>
2018-10-04 09:15:04 -0400

    Housekeeping

--- src/containers/Actors/Actors.js
+++ src/containers/Actors/Actors.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 
 class Actors extends Component {
-    render () {
+    render() {
         return (
             <div>
                 <h1>The Actors Page</h1>
